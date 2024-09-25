25fcbbc0904f9ca1d26f38bb985dc8e4d0e96107
unknown <carolinehoeg123@gmail.com>
2024-09-25 19:08:47 +0200

    logic

---
+++ SS_OLA2_BACKEND/Data/Job.cs
@@ -0,0 +1,11 @@
+﻿namespace SS_OLA2_BACKEND.Data
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public Warehouse StorageLocation { get; set; }
+        public DateTime Date { get; set; }
+        public JobType JobType { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ SS_OLA2_BACKEND/JobManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SS_OLA2_BACKEND.Data;
+
+namespace SS_OLA2_BACKEND
+{
+    public class JobManager
+    {
+        private static Random random = new Random();
+        // add db connection
+        public JobManager() { }
+
+        // get shipment info about job type
+        public Job CreateJob(JobType jobType) 
+        {
+            int randomWarehouseId = random.Next(1, 6);
+
+            Job job = new Job() 
+            {
+                StorageLocation = new Warehouse(randomWarehouseId),
+                Date = DateTime.Now,
+                JobType = jobType,
+                Status = "Pending"
+            };
+
+            // save in db
+            return job; 
+        }
+
+    }
+}
