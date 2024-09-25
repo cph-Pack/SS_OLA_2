36a0610dbdd4a2b3001962bbca431aa00db2f845
albiek1 <54135401+albiek1@users.noreply.github.com>
2024-09-25 21:06:51 +0200

    Fixed tickets

--- SS_OLA2_BACKEND/Data/Ticket.cs
+++ SS_OLA2_BACKEND/Data/Ticket.cs
@@ -10,10 +10,24 @@ namespace SS_OLA2_BACKEND.Data
         public string LastName { get; set; }
         [Required]
         public string CompanyName { get; set; }
+
         [Required]
+        public Chemicals Chemicals { get; set; }
+
+        /*[Required]
         public string ChemicalType { get; set; }
         [Required]
-        public int KilogramWeight { get; set; }
+        public int KilogramWeight { get; set; }*/
     }
     
+    public class Chemicals
+    {
+        [Required]
+        public int Type_A { get; set; } = 0;
+
+        [Required]
+        public int Type_B { get; set;} = 0;
+
+        [Required]
+        public int Type_C { get; set;} = 0;
 }
